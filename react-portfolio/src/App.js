81dbc9098040162a006e542fb687f516ca6d2115
Akshata <akshata.dhuraji@gmail.com>
2022-11-08 12:22:12 +1100

    Working without firebase. Still needa sort out portfolio , contact form and map stuff

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+//using scss as using sas extension for reducing amount of garbage css code written hehe
+import { Route, Routes } from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+import Layout from './components/Layout'
+import Portfolio from './components/Portfolio'
+//import Dashboard from './components/Dashboard'
+import './App.scss'
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+
+        </Route>
+      </Routes>
+    </>
+  )
+}
+
+export default App
+
+// <Route path="/dashboard" element={<Dashboard />} />
---
+++ react-portfolio/src/components/About/index.js
@@ -0,0 +1,75 @@
+import { useEffect, useState } from 'react'
+import {
+  faAngular,
+  faCss3,
+  faGitAlt,
+  faHtml5,
+  faJsSquare,
+  faReact,
+} from '@fortawesome/free-brands-svg-icons'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import './index.scss'
+
+const About = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  useEffect(() => {
+     setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 4000)
+  }, [])
+
+  return (
+    <>
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            I'm an avid coder and extremely passionate about all things tech!!
+            I'm super interested in the data space (specially data engineering)
+            and full stack dev!
+          </p>
+          <p align="LEFT">
+            I am super passionate about seeing more female representation in
+            tech!! Apart from this, I'm also into dancing and love watching TV
+            shows (Modern Family , GOT, AHS,  Gossip Girl xoxo)
+          </p>
+        </div>
+
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faAngular} color="#DD0031" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
+}
+
+export default About
